f295dec079cf47e6b3330f5d6597376b715fe84f
GUILHERME LUIZ MARTINS DE OLIVEIRA <guilhermesky123@gmail.com>
2024-10-03 21:36:58 -0300

    finalização

--- js/script.js
+++ js/script.js
@@ -10,6 +10,11 @@ btnAdd.addEventListener('click', function(){
     addTask();
 })
 
+inputText.addEventListener('keydown', function (e){ 
+    if(e.code == "Enter")
+    addTask();
+})
+
 // * Função - conectada ao botão
 function addTask(){
 // ^ valeu em um input = Valor digitado ou valor dentro da caixa de texto
@@ -28,9 +33,54 @@ function addTask(){
         // * appendChild = adicionar uma tag dentro de outra 
         // * no caso o novo <li> é colocado dentro do <ul> taskList
         tasKList.appendChild(li);
+
+        // * criando um span com x
+        let span = document.createElement('span');
+        // * \u007 representa um x
+        span.innerHTML = '\u00d7';
+        // ^ Adicionando o span DENTRO do <li> que acabou de ser criando
+        li.appendChild(span);
     }
 
     // * Limpando caixa de texto para a próxima tarefa
     inputText.value = '';
+    saveData();
 
 }
+
+// * 03/10/2024
+
+tasKList.addEventListener('click', function(e){
+    // ^ O 'e' função anônima representa a TAG que foi clicada pois dentro da TaskLIST TEM MUITAS COISAS para clicar
+    // ^ (ul, span, li imagem) entao saber no que exatamente foi clicado
+
+    // *  Chegando se o que foi clicado foi a tag <li>
+    if(e.target.tagName == 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    } 
+    else if (e.target.tagName == 'SPAN'){
+// * pegamos o que foi clicado e acessamos o seu  PAI 
+
+
+// ^ e apagamos o seu pai (O <LI>)
+// * e.target = tagSpan
+        e.target.parentElement.remove();
+        saveData();
+    }
+})
+// * salvando informação no navegador, para não peder as terefas ao fechar o site.
+
+function saveData(){
+    localStorage.setItem("task_data", tasKList.innerHTML);
+}
+
+function loadData(){
+    // * A o carregar o site de novo, o <ul> taskList vai iniciar vaio
+    // * então escrevemos o seu innerHTML (seu conteúdo) com a informação salva no localStorage
+    tasKList.innerHTML = localStorage.getItem('task_data');
+
+    // ^ Chmando a função fora = chama ao carregar o site
+    // *  então ao abrir o site ele carrega os <li> salvamos no localStorage
+}
+loadData();
